a43270e1aaf2322d264dbb7766e77ba6d02943a7
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-09-24 17:09:09 +0300

    added spinner before render calls and contacts

--- src/components/Contacts/ListContacts/ListContacts.jsx
+++ src/components/Contacts/ListContacts/ListContacts.jsx
@@ -5,34 +5,44 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchContactList } from '../../../redux/users/user-operation';
 import { getContacts } from '../../../redux/users/user-selector';
+import SpinnerItem from '../../../components/Spinner/SpinnerItem.jsx';
 
 import './ListContacts.scss';
 
 const ListContacts = (props) => {
   const dispatch = useDispatch();
+  
   useEffect(() => {
     dispatch(fetchContactList());
   }, [dispatch]);
 
   const allContacts = useSelector(getContacts);
+  const checkStore = allContacts.length !== 0;
 
-  let contactElements = allContacts.map(
-    ({ id, first_name, last_name, phone, favorite }) => (
-      <ItemContact
-        key={id}
-        firstName={first_name}
-        lastName={last_name}
-        phone={phone}
-        favorite={favorite}
-      />
-    )
-  );
+  if (checkStore) {
+    let contactElements = allContacts.map(
+      ({ id, first_name, last_name, phone, favorite }) => (
+        <ItemContact
+          key={id}
+          firstName={first_name}
+          lastName={last_name}
+          phone={phone}
+          favorite={favorite}
+        />
+      )
+    );
+  
+    return (
+      <div className='contacts-list-section'>
+        <ul className='list-contacts'>{contactElements}</ul>
+      </div>
+    );
 
-  return (
-    <div className='contacts-list-section'>
-      <ul className='list-contacts'>{contactElements}</ul>
-    </div>
-  );
+  }else{
+    return <SpinnerItem />;
+  }
+
+
 };
 
 export default ListContacts;
