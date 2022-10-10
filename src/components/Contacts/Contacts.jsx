a7bc3322b02fe3e44017c6f2b8755dc104a939e5
Dmitriy Artemmov <dmitriy.a@doubledmarketing.com>
2022-10-10 17:23:03 +0300

    [artemov] Added content - ProfileCall

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './Contacts.scss';
 import Search from '../UI/Search/Search';
 import ListContacts from './ListContacts/ListContacts';
+import ProfileContacts from './ProfileContacts/ProfileContacts';
 
 const Contacts = () => {
   return (
-    <section className='left-bar'>
-      <Search />
-      <ListContacts />
-    </section>
+    <>
+      <section className='left-bar'>
+        <Search />
+        <ListContacts />
+      </section>
+
+      <ProfileContacts />
+    </>
   );
 };
 
